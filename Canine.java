7d1a8ec15cf6e54fa715986cbc8fb1c40414a57e
Hannah <han.lou.mitchell@gmail.com>
2018-03-11 14:17:08 -0700

    added more classes

--- Canine.java
+++ Canine.java
@@ -2,17 +2,30 @@ import java.util.*;
 
 public class Canine extends Carnivore
 {
-    //blank constructor
+    //default constructor
     Canine()
     {
         super();
+        species = "Canine";
     }
     
     //overloaded constructor accepts all canine attributes
     Canine(String name, String location, int age, double weight, Health health,
-        boolean endangered, String preferredMeat, double dailyVolume)
+        String preferredMeat, double dailyVolume)
     {
-        super(name, location, age, weight, health, endangered, preferredMeat, 
-            dailyVolume);
+        super(name, location, age, weight, health, preferredMeat, dailyVolume);
+        species = "Canine";
+    }
+
+    //calls to print header listing carnivore attribute categories
+    public void printHeader()
+    {
+        super.printHeader();
+    }
+
+    //calls to print carnivore attributes
+    public void printInfo()
+    {
+        super.printInfo();
     }
 }
